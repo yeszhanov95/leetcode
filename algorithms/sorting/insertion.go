8879e370d2cd20edf790f6dc30021360f15d0e29
Assylkhan <asil_24_95@mail.ru>
2020-08-26 22:28:17 +0600

    insertion sort modified

--- algorithms/sorting/insertion.go
+++ algorithms/sorting/insertion.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	arr := []int{ 2, 5, 2, 8, 6, 9 }
+	arr := []int{ 2, 5, -1, 8, 6, 9 }
 	fmt.Println(arr)
 	sorted := insertionSort(arr)
 	fmt.Println(sorted)
@@ -15,7 +15,7 @@ func insertionSort(arr []int) []int {
 	copy(result, arr)
 
 	for i := 1; i < length; i++ {
-		for j := i; j >= 0; j-- {
+		for j := i; j > 0; j-- {
 			if result[j] >= result[j-1] {
 				break
 			}
