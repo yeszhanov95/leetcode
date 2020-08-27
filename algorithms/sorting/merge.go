87dfaa4a44cdcedc60a95120c329ad1f485d22fa
Assylkhan <asil_24_95@mail.ru>
2020-08-27 16:21:49 +0600

    merge sort

--- algorithms/sorting/merge.go
+++ algorithms/sorting/merge.go
@@ -3,57 +3,42 @@ package main
 import "fmt"
 
 func main() {
-	slice := []int{2, 9, -1, 5, 6, 0}
+	slice := []int{2, 9, -1, 0, -3, 32, 4, -843}
 	fmt.Println(slice)
 	sorted := mergeSort(slice)
 	fmt.Println(sorted)
 }
 
 func mergeSort(slice []int) []int {
-	result := make([]int, len(slice))
-	copy(result, slice)
-	subSort(result, 0, len(slice) - 1)
-	return result
-}
-
-func subSort(slice []int, leftStart, rightEnd int) {
-	fmt.Println("subSort:", leftStart, rightEnd)
-	if leftStart >= rightEnd {
-		return
+	length := len(slice)
+	if length < 2 {
+		return slice
 	}
 
-	middle := (leftStart + rightEnd) / 2
-	subSort(slice, leftStart, middle)
-	subSort(slice, middle + 1, rightEnd)
-
-	merge(slice, leftStart, rightEnd)
+	m := length / 2
+	return merge(mergeSort(slice[:m]), mergeSort(slice[m:]))
 }
 
-func merge(slice []int, leftStart, rightEnd int) {
-	fmt.Println("merge:", leftStart, rightEnd)
-	tmp := make([]int, len(slice))
-	copy(tmp, slice)
+func merge(leftSlice, rightSlice []int) []int {
+	leftLen, rightLen := len(leftSlice), len(rightSlice)
+	size, leftIdx, rightIdx := leftLen + rightLen, 0, 0
+	slice := make([]int, size)
 
-	leftEnd := (leftStart + rightEnd) / 2
-	rightStart := leftEnd + 1
-
-	left := leftStart
-	right := rightStart
-	index := leftStart
-
-	for left <= leftEnd && right <= rightEnd {
-		if slice[left] > slice[right] {
-			tmp[index] = slice[right]
-			right++
+	for k := 0; k < size; k++ {
+		if leftIdx > leftLen - 1 {
+			slice[k] = rightSlice[rightIdx]
+			rightIdx++
+		} else if rightIdx > rightLen - 1 {
+			slice[k] = leftSlice[leftIdx]
+			leftIdx++
+		} else if leftSlice[leftIdx] < rightSlice[rightIdx] {
+			slice[k] = leftSlice[leftIdx]
+			leftIdx++
 		} else {
-			tmp[index] = slice[left]
-			left++
+			slice[k] = rightSlice[rightIdx]
+			rightIdx++
 		}
-		index++
 	}
 
-	copy(tmp[index : rightEnd+1], slice[right : rightEnd + 1])
-	copy(tmp[index : rightEnd+1], slice[left : leftEnd + 1])
-
-	copy(slice, tmp)
+	return slice
 }
