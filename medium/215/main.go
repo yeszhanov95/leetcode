bff8fef2acab1904b535de2ec8f8ab177100d4ee
Assylkhan <asil_24_95@mail.ru>
2020-09-15 19:53:32 +0600

    215. Kth Largest Element in an Array

--- medium/215/main.go
+++ medium/215/main.go
@@ -1,10 +1,33 @@
 package main
 
-import "sort"
+import "container/heap"
 
 func findKthLargest(nums []int, k int) int {
-	sort.Slice(nums, func(i, j int) bool {
-		return nums[i] < nums[j]
-	})
-	return nums[len(nums) - k]
+	heapArr := make(HeapArr, len(nums))
+	copy(heapArr, nums)
+	heap.Init(&heapArr)
+	var res int
+	for ; k > 0; k-- {
+		res = heap.Pop(&heapArr).(int)
+	}
+	return res
+}
+
+type HeapArr []int
+
+func (h HeapArr) Len() int { return len(h) }
+
+func (h HeapArr) Less(i, j int) bool { return h[i] > h[j] }
+
+func (h HeapArr) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
+
+func (h *HeapArr) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *HeapArr) Pop() interface{} {
+	old := *h
+	last := old[len(old)-1]
+	*h = old[:len(old)-1]
+	return last
 }
