96bb7f6902c11c17fa3ba9ae945668b1eff5b2a1
Assylkhan <asil_24_95@mail.ru>
2020-09-19 16:42:38 +0600

    415. Add Strings

--- easy/415/main.go
+++ easy/415/main.go
@@ -1,53 +1,23 @@
 package main
 
 func addStrings(num1 string, num2 string) string {
-	zeroVal := []byte("0")[0]
-	cacheNums, cacheChars := make(map[byte]int, 10), make(map[int]byte, 10)
-	for i := 0; i < 10; i++ {
-		c := zeroVal + byte(i)
-		cacheNums[c] = i
-		cacheChars[i] = c
+	res := make([]byte, len(num1)+1)
+	if len(num1) < len(num2) {
+		res = make([]byte, len(num2)+1)
 	}
 
-	result := make([]byte, len(num1) + len(num2))
-	idx, i, j, remainder := len(result) - 1, len(num1)-1, len(num2)-1, 0
-
-	for ; i >= 0 && j >= 0; i, j, idx = i-1, j-1, idx-1 {
-		num := cacheNums[num1[i]] + cacheNums[num2[j]] + remainder
-		result[idx] = cacheChars[num % 10]
-		if num >= 10 {
-			remainder = 1
-		} else {
-			remainder = 0
+	for carry, i, j, k := byte(0), len(num1)-1, len(num2)-1, len(res)-1; k >= 0; carry, i, j, k = carry/10, i-1, j-1, k-1 {
+		if i >= 0 {
+			carry += num1[i] - '0'
 		}
-	}
-
-	if i < 0 {
-		for ; j >= 0; j, idx = j-1, idx-1 {
-			num := cacheNums[num2[j]] + remainder
-			result[idx] = cacheChars[num % 10]
-			if num >= 10 {
-				remainder = 1
-			} else {
-				remainder = 0
-			}
+		if j >= 0 {
+			carry += num2[j] - '0'
 		}
-	} else if j < 0 {
-		for ; i >= 0; i, idx = i-1, idx-1 {
-			num := cacheNums[num1[i]] + remainder
-			result[idx] = cacheChars[num % 10]
-			if num >= 10 {
-				remainder = 1
-			} else {
-				remainder = 0
-			}
-		}
+		res[k] = (carry % 10) + '0'
 	}
 
-	if remainder > 0 {
-		result[idx] = cacheChars[1]
-		idx--
+	if res[0] == '0' {
+		return string(res[1:])
 	}
-
-	return string(result[idx+1:])
+	return string(res)
 }
