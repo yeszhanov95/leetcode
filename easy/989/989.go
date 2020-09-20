911f40da98f5eb4613a1f537a935973aa1cfd75f
Assylkhan <asil_24_95@mail.ru>
2020-09-20 13:26:35 +0600

    989. Add to Array-Form of Integer

--- easy/989/989.go
+++ easy/989/989.go
@@ -4,18 +4,18 @@ import "strconv"
 
 func addToArrayForm(A []int, K int) []int {
 	res := make([]int, len(A)+1)
-	S := strconv.Itoa(K)
-	if len(S) > len(A) {
-		res = make([]int, len(S)+1)
+	kLen := len(strconv.Itoa(K))
+	if kLen > len(A) {
+		res = make([]int, kLen+1)
 	}
-	res[0] = -1
 
-	for carry, i, j, k := 0, len(A)-1, len(S)-1, len(res)-1; k >= 0; carry, i, j, k = carry/10, i-1, j-1, k-1 {
+	for carry, i, k := 0, len(A)-1, len(res)-1; k >= 0; carry, i, k = carry/10, i-1, k-1 {
 		if i >= 0 {
 			carry += A[i]
 		}
-		if j >= 0 {
-			carry += int(S[j] - '0')
+		if K > 0 {
+			carry += K % 10
+			K /= 10
 		}
 		res[k] = carry % 10
 	}
