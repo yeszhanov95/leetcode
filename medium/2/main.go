16ca3edbc01ec49739ffc7db9c973a33a9abddcb
Assylkhan <asil_24_95@mail.ru>
2020-09-20 18:17:33 +0600

    2. Add Two Numbers

--- medium/2/main.go
+++ medium/2/main.go
@@ -6,25 +6,17 @@ type ListNode struct {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	slice := make([]int, 0)
-	for carry := 0; l1 != nil || l2 != nil || carry > 0; carry = carry/10 {
-		if l1 != nil {
-			carry += l1.Val
-			l1 = l1.Next
-		}
-		if l2 != nil {
-			carry += l2.Val
-			l2 = l2.Next
-		}
-		slice = append(slice, carry % 10)
-	}
-
 	res := &ListNode{}
 	node := res
-	for _, v := range slice {
-		node.Next = &ListNode{Val:v}
+	for carry := 0; l1 != nil || l2 != nil || carry > 0; carry = carry/10 {
+		if l1 != nil {
+			carry, l1 = carry + l1.Val, l1.Next
+		}
+		if l2 != nil {
+			carry, l2 = carry + l2.Val, l2.Next
+		}
+		node.Next = &ListNode{Val: carry % 10}
 		node = node.Next
 	}
-
 	return res.Next
 }
