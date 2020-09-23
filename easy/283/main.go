4825335e9ce8359d688e8de08495d666706a2bf8
Assylkhan <asil_24_95@mail.ru>
2020-09-23 21:50:15 +0600

    283. Move Zeroes

--- easy/283/main.go
+++ easy/283/main.go
@@ -1,14 +1,17 @@
 package main
 
 func moveZeroes(nums []int)  {
-	idx := 0
-	for _, v := range nums {
-		if v != 0 {
-			nums[idx] = v
-			idx++
+	for i, j := 0, 1; i < len(nums) && j < len(nums); i++ {
+		if nums[i] == 0 {
+			for ; j < len(nums); j++ {
+				if nums[j] != 0 {
+					nums[i], nums[j] = nums[j], nums[i]
+					j++
+					break
+				}
+			}
+		} else {
+			j++
 		}
-	}
-	for ; idx < len(nums); idx++ {
-		nums[idx] = 0
 	}
 }
