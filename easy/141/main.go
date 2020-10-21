8a32b73386512b486326499452a4addb6ca517a0
Assylkhan <asil_24_95@mail.ru>
2020-10-21 16:27:54 +0600

    141. Linked List Cycle

---
+++ easy/141/main.go
@@ -0,0 +1,22 @@
+package main
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+func hasCycle(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+
+	slow, fast := head, head.Next
+	for fast != nil && fast.Next != nil {
+		if slow == fast {
+			return true
+		}
+		slow, fast = slow.Next, fast.Next.Next
+	}
+
+	return false
+}
