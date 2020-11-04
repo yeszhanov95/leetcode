c29ac0b234c775a961a16779e199996432d6ae0a
Assylkhan <asil_24_95@mail.ru>
2020-11-04 14:06:42 +0600

    130. Surrounded Regions

--- medium/130/main.go
+++ medium/130/main.go
@@ -1,42 +1,34 @@
 package main
 
 func solve(board [][]byte)  {
-	visited := make([][]bool, 0, len(board))
-	for i := len(board); i > 0; i-- {
-		visited = append(visited, make([]bool, len(board[0])))
+	if len(board) == 0 { return }
+	rows, cols := len(board), len(board[0])
+	for c := 0; c < cols; c++ {
+		dfs(0, c, &board)
+		if rows > 1 { dfs(rows - 1, c, &board) }
 	}
-
-	var rows, cols []int
-	var onBorder bool
+	for r := 1; r < rows; r++ {
+		dfs(r, 0, &board)
+		if cols > 1 { dfs(r, cols - 1, &board) }
+	}
 	for i := range board {
 		for j := range board[i] {
-			if visited[i][j] || board[i][j] == 'X' { continue }
-			rows, cols = make([]int, 0), make([]int, 0)
-			onBorder = false
-			dfs(i, j, board, visited, &rows, &cols, &onBorder)
-
-			if !onBorder && len(rows) > 0 {
-				for k := 0; k < len(rows); k++ {
-					board[rows[k]][cols[k]] = 'X'
-				}
+			if board[i][j] == 'O' {
+				board[i][j] = 'X'
+			} else if board[i][j] == 'Z' {
+				board[i][j] = 'O'
 			}
 		}
 	}
 }
 
-func dfs(i, j int, board [][]byte, visited [][]bool, rows, cols *[]int, onBorder *bool) {
-	if i < 0 || j < 0 || i == len(board) || j == len(board[0]) || board[i][j] == 'X' || visited[i][j] {
+func dfs(i, j int, board *[][]byte) {
+	if i < 0 || j < 0 || i == len(*board) || j == len((*board)[0]) || (*board)[i][j] != 'O' {
 		return
 	}
-
-	visited[i][j] = true
-	*rows, *cols = append(*rows, i), append(*cols, j)
-	if !(*onBorder) && (i == 0 || j == 0 || i == len(board) - 1 || j == len(board[0]) - 1) {
-		*onBorder = true
-	}
-
-	dfs(i + 1, j, board, visited, rows, cols, onBorder)
-	dfs(i - 1, j, board, visited, rows, cols, onBorder)
-	dfs(i, j + 1, board, visited, rows, cols, onBorder)
-	dfs(i, j - 1, board, visited, rows, cols, onBorder)
+	(*board)[i][j] = 'Z'
+	dfs(i + 1, j, board)
+	dfs(i - 1, j, board)
+	dfs(i, j + 1, board)
+	dfs(i, j - 1, board)
 }
