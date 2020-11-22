819cf16571b068238515eb08785dd5e0950b3de4
Assylkhan <asil_24_95@mail.ru>
2020-11-22 20:23:32 +0600

    235. Lowest Common Ancestor of a Binary Search Tree

---
+++ easy/235/main.go
@@ -0,0 +1,20 @@
+package main
+
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	for {
+		if root.Val > p.Val && root.Val > q.Val {
+			root = root.Left
+		} else if root.Val < p.Val && root.Val < q.Val {
+			root = root.Right
+		} else {
+			break
+		}
+	}
+	return root
+}
