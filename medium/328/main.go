3d3b99c6c9a7d83630be4ecb9de55dedca432317
Assylkhan <asil_24_95@mail.ru>
2021-01-05 19:06:54 +0600

    328. Odd Even Linked List

---
+++ medium/328/main.go
@@ -0,0 +1,21 @@
+package main
+
+type ListNode struct {
+	Next *ListNode
+}
+
+func oddEvenList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil || head.Next.Next == nil {
+		return head
+	}
+	evenHead := head.Next
+	odd, even := head, evenHead
+	for even != nil && even.Next != nil {
+		odd.Next = even.Next
+		odd = odd.Next
+		even.Next = odd.Next
+		even = even.Next
+	}
+	odd.Next = evenHead
+	return head
+}
