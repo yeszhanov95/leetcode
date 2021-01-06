7720a7803754a332c36276928be491d4ab74c9d4
Assylkhan <asil_24_95@mail.ru>
2021-01-06 13:31:20 +0600

    171. Excel Sheet Column Number

--- easy/171/main.go
+++ easy/171/main.go
@@ -1,16 +1,10 @@
 package main
 
 func titleToNumber(s string) int {
-	n := len(s)
-	var columnNumber int
-	for i := 0; i < n - 1; i++ {
-		left := int(s[i] - 'A') + 1
-		right := 1
-		for j := n - i - 1; j > 0; j-- {
-			right *= 26
-		}
-		columnNumber += left * right
+	columnNumber, n := 0, 1
+	for i := len(s) - 1; i >= 0; i-- {
+		columnNumber += int(s[i] - 'A' + 1) * n
+		n *= 26
 	}
-	columnNumber += int(s[n-1] - 'A') + 1
 	return columnNumber
 }
